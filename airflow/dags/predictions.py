15535f2866955875fefb8eef2e59a4cd10897fd2
Mauro Del Rio <mauro@crs4.it>
2021-03-12 23:36:21 +0100

    add filtering and fix output

--- airflow/dags/predictions.py
+++ airflow/dags/predictions.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from airflow.providers.docker.operators.docker import DockerOperator
 
 from airflow import DAG
+
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
@@ -18,19 +19,24 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+base_image = 'slaid:0.11.0-ref_batches'
+tissue_image = f'{base_image}-tissue_model-extract_tissue_eddl_1.1'
+tumor_image = f'{base_image}-tumor_model-promort_vgg16_weights_ep_41_vacc_0.91'
+
 with DAG('predictions',
          default_args=default_args,
          schedule_interval=None,
          catchup=False) as dag:
-    t1 = DockerOperator(
-        task_id='tissue_8',
-        image='mobydick.crs4.it:5000/slaid:'
-        '0.9.2-ref_lazy-net-creation-tissue_model-extract_tissue_eddl_1.1',
+
+    tissue_low_res = DockerOperator(
+        task_id='tissue_low_res',
+        image=tissue_image,
         api_version='auto',
         auto_remove=True,
         #  force_pull=True,
         user='root',
-        command='parallel  -f tissue -o {{ dag_run.conf["output"] }} --overwrite'
+        command=
+        'parallel  -f tissue_8 -o {{ dag_run.conf["output"] }} --overwrite'
         ' {{ "--gpu" if "gpu" in dag_run.conf else ""  }} '
         ' {{ dag_run.conf["gpu"] if "gpu" in dag_run.conf  else ""  }} '
         ' {{ "-b" if "tissue-batch" in dag_run.conf else ""  }} '
@@ -39,7 +45,49 @@ with DAG('predictions',
         ' {{ dag_run.conf["tissue-level"] if "tissue-level" in dag_run.conf  else ""  }} '
         ' {{ "--scheduler"   if "scheduler"  in dag_run.conf else "" }} '
         ' {{   dag_run.conf["scheduler"] if "scheduler"  in dag_run.conf else "" }} '
-        ' {{ dag_run.conf["slide"]["path"] }}',
+        ' {{ dag_run.conf["slide"]["dirname"] }}/{{ dag_run.conf["slide"]["filename"] }}',
         volumes=['/mnt/tdm-dic:/mnt/tdm-dic'],
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge")
+
+    tissue_high_res = DockerOperator(
+        task_id='tissue_high_res',
+        image=tissue_image,
+        api_version='auto',
+        auto_remove=True,
+        #  force_pull=True,
+        user='root',
+        command='parallel  -f tissue -o {{ dag_run.conf["output"] }} --overwrite'
+        ' {{ "--gpu" if "gpu" in dag_run.conf else ""  }} '
+        ' {{ dag_run.conf["gpu"] if "gpu" in dag_run.conf  else ""  }} '
+        ' -F {{ dag_run.conf["tissue-label"]   }}>0.1 '
+        ' -l 0'
+        ' {{ "--scheduler"   if "scheduler"  in dag_run.conf else "" }} '
+        ' {{   dag_run.conf["scheduler"] if "scheduler"  in dag_run.conf else "" }} '
+        ' {{ dag_run.conf["output"] }}/{{ dag_run.conf["slide"]["filename"] }}.zarr',
+        volumes=['/mnt/tdm-dic:/mnt/tdm-dic'],
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge")
+
+    tumor = DockerOperator(
+        task_id='tumor',
+        image=tumor_image,
+        api_version='auto',
+        auto_remove=True,
+        #  force_pull=True,
+        user='root',
+        command=
+        'parallel  -f {{ dag_run.conf["tumor-label"] }} -o {{ dag_run.conf["output"] }} --overwrite'
+        ' {{ "--gpu" if "gpu" in dag_run.conf else ""  }} '
+        ' {{ dag_run.conf["gpu"] if "gpu" in dag_run.conf  else ""  }} '
+        '-b 5000'
+        ' -F {{ dag_run.conf["tissue-label"]   }}>0.6 '
+        ' -l {{ dag_run.conf["tumor-level"] }}'
+        ' {{ "--scheduler"   if "scheduler"  in dag_run.conf else "" }} '
+        ' {{   dag_run.conf["scheduler"] if "scheduler"  in dag_run.conf else "" }} '
+        ' {{ dag_run.conf["output"] }}/{{ dag_run.conf["slide"]["filename"] }}.zarr',
+        volumes=['/mnt/tdm-dic:/mnt/tdm-dic'],
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge")
+
+    tissue_low_res >> [tumor, tissue_high_res]
