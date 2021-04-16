71b844f2860294f4d7986dab8611633af095935e
Mauro Del Rio <mauro@crs4.it>
2021-04-16 14:50:08 +0200

    clean workspace

--- airflow/dags/watch_dir.py
+++ airflow/dags/watch_dir.py
@@ -2,21 +2,16 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
-import shutil
 import time
 from datetime import datetime, timedelta
-from typing import List
 
 import yaml
 from airflow.api.common.experimental.trigger_dag import trigger_dag
-from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.decorators import task
 from airflow.models import DagRun, Variable
 #  from airflow.operators.bash import BashOperator
 from airflow.operators.python import get_current_context
 from airflow.utils import timezone
-from airflow.utils.dates import days_ago
-from airflow.utils.state import State
 from airflow.utils.types import DagRunType
 
 from airflow import DAG
@@ -34,31 +29,12 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-with DAG('watch_dir',
-         default_args=default_args,
-         schedule_interval='* * * * *',
-         start_date=datetime(2019, 1, 1),
-         catchup=False,
-         max_active_runs=1) as dag:
-    file_sensor = FileSensor(task_id="file_sensor",
-                             poke_interval=5,
-                             filepath='{{  var.value.dropbox }}')
+with DAG('watch_dir', default_args=default_args,
+         schedule_interval=None) as dag:
 
     @task
-    def move_files() -> List[str]:
-        dropbox = Variable.get('dropbox')
-        stage = Variable.get('stage')
-        incoming_files = os.listdir(dropbox)
-        for fname in incoming_files:
-            logger.info('moving %s/%s to %s', dropbox, fname, stage)
-            try:
-                shutil.move(os.path.join(dropbox, fname), stage)
-            except Exception as ex:
-                logger.error(ex)
-        return incoming_files
-
-    @task
-    def trigger_predictions(incoming_files: List[str]):
+    def trigger_predictions():
+        incoming_files = get_current_context()['params']['slides']
         logger.info("incoming_files %s", incoming_files)
         stage = Variable.get('stage')
         output = Variable.get('output')
@@ -82,5 +58,4 @@ with DAG('watch_dir',
                             replace_microseconds=False)
                 time.sleep(1)
 
-    incoming_files = move_files()
-    file_sensor >> incoming_files >> trigger_predictions(incoming_files)
+    trigger_predictions()
