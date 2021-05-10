d7b94208b88e03630cd4683a26dadd5f71869b2a
Mauro Del Rio <mauro@crs4.it>
2021-05-10 15:47:35 +0200

    add sleep in init script for waiting sql tables to be created

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,3 @@
+sleep 10
 airflow users create  --username $AIRFLOW_USER --firstname firstname --lastname lastname --role Admin --email firstname@lastname.org -p $AIRLFLOW_PASSWORD
 airflow variables set STAGE  $STAGE_DIR
