d358befd937dd980ddd6e3fcf21851cae46f1de2
Mauro Del Rio <mauro@crs4.it>
2021-11-24 17:06:00 +0100

    add env variables for building cwl-airflow (repo url, branch, extra pip dependencies)

--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -xe
 
 echoerr() { echo "$@" 1>&2; }
 
@@ -32,9 +33,27 @@ chmod a+w $FAILED_DIR
 mkdir -p $BACKUP_DIR
 chmod a+w $BACKUP_DIR
 
+python -c "\
+import toml
+dependencies = toml.load(open('pyproject.toml'))['tool']['poetry']['dependencies']
+
+cw_airflow = dependencies.pop('cwl-airflow')
+git_repo = cw_airflow['git']
+rev = cw_airflow['rev']
+print(f'CWL_AIRFLOW_VERSION={rev}')
+print(f'CWL_AIRFLOW_URL={git_repo}')
+
+dependencies.pop('python')
+extra_pip_deps = []
+for k, v in dependencies.items():
+  extra_pip_deps.append(f'{k}=={v.strip(\"^\")}')
+print(f'EXTRA_PIP_DEPS={\" \".join(extra_pip_deps)}')
+" >> .env
+source .env
+
 CWL_AIRFLOW_DIR=build/cwl-airflow
 if [ ! -d $CWL_AIRFLOW_DIR ]; then
-  git clone https://github.com/mdrio/cwl-airflow.git $CWL_AIRFLOW_DIR
+  git clone $CWL_AIRFLOW_URL --branch $CWL_AIRFLOW_VERSION $CWL_AIRFLOW_DIR
 else
    echoerr "cwl aiflow found, skip cloning"
 fi
