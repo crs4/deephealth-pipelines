7da6ccef04ba51ca7d704469eec2bb2ec8a5f15e
Mauro Del Rio <mauro@crs4.it>
2021-11-26 18:47:50 +0100

    add script for testing pipeline

---
+++ tests/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+./create_env.sh
+source .env
+./compose.sh up -d
+
+running=$(docker-compose ps --services --filter "status=running" | grep init)
+while [ $running ]; do
+  echo waiting for init to complete
+  sleep 5
+done
+
+cp -r tests/data/Mirax2-Fluorescence-2* $INPUT_DIR
+
+cd slide-importer
+poetry shell
+python slide_importer/local.py  --user $AIRFLOW_USER -P $AIRFLOW_PASSWORD --server-url http://localhost:8080  -p '{ "tissue-high-level": 8, "tissue-high-filter": "tissue_low>1", "tumor-filter": "tissue_low>1", "gpu": null,"tumor-chunk": 1024}' --wait
