35b92783dbc82092a68cc08681015d1c82896869
Mauro Del Rio <mauro@crs4.it>
2021-11-26 19:09:08 +0100

    fix test script

--- tests/run.sh
+++ tests/run.sh
@@ -14,5 +14,6 @@ done
 cp -r tests/data/Mirax2-Fluorescence-2* $INPUT_DIR
 
 cd slide-importer
+poetry install
 poetry shell
 python slide_importer/local.py  --user $AIRFLOW_USER -P $AIRFLOW_PASSWORD --server-url http://localhost:8080  -p '{ "tissue-high-level": 8, "tissue-high-filter": "tissue_low>1", "tumor-filter": "tissue_low>1", "gpu": null,"tumor-chunk": 1024}' --wait
