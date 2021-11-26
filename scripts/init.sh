dd5c4b6796f88c50735ffa5ce5584bc6708cf4c1
Mauro Del Rio <mauro@crs4.it>
2021-11-26 12:06:52 +0100

    make airflow user and password configurable via .env - fix typo

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 set -ex
 
 sleep 10
-airflow users create  --username $AIRFLOW_USER --firstname firstname --lastname lastname --role Admin --email firstname@lastname.org -p $AIRLFLOW_PASSWORD
+airflow users create  --username $AIRFLOW_USER --firstname firstname --lastname lastname --role Admin --email firstname@lastname.org -p $AIRFLOW_PASSWORD
 
 airflow variables set INPUT_DIR  $INPUT_DIR
 airflow variables set STAGE_DIR  $STAGE_DIR
