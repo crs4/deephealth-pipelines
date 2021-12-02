dcea223943c0aea2c3006f0088b38e79f4363b06
Mauro Del Rio <mauro@crs4.it>
2021-12-02 19:25:54 +0100

    debug run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -24,18 +24,19 @@ done
 
 ./compose.sh ps
 ./compose.sh logs promort-web
-# ome_sedragon_status=$(check_omeseadragon $OME_SEADRAGON_URL)
-#
-# echo $ome_sedragon_status
-# while [ $ome_sedragon_status -ne 0 ]; do
-#   echo waiting for omeseadragon to be up and running
-#   sleep 5
-#   ome_sedragon_status=$(check_omeseadragon $OME_SEADRAGON_URL)
-# done
-#
-#
-# cd slide-importer
-# poetry install
-# poetry shell
-# set -e
-# python slide_importer/local.py  --user $AIRFLOW_USER -P $AIRFLOW_PASSWORD --server-url http://localhost:8080  -p '{ "tissue-high-level": 8, "tissue-high-filter": "tissue_low>1", "tumor-filter": "tissue_low>1", "gpu": null,"tumor-chunk": 1024}' --wait
+ome_sedragon_status=$(check_omeseadragon $OME_SEADRAGON_URL)
+
+echo $ome_sedragon_status
+while [ $ome_sedragon_status -ne 0 ]; do
+  echo waiting for omeseadragon to be up and running
+  sleep 5
+  ome_sedragon_status=$(check_omeseadragon $OME_SEADRAGON_URL)
+done
+
+
+cd slide-importer
+pip install poetry
+poetry shell
+poetry install
+set -e
+python slide_importer/local.py  --user $AIRFLOW_USER -P $AIRFLOW_PASSWORD --server-url http://localhost:8080  -p '{ "tissue-high-level": 8, "tissue-high-filter": "tissue_low>1", "tumor-filter": "tissue_low>1", "gpu": null,"tumor-chunk": 1024}' --wait
