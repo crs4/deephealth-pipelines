9053d6d2978c49b35bce181ca9d209c04785d75e
Mauro Del Rio <mauro@crs4.it>
2021-12-23 15:37:29 +0100

    add check for ro-crate in tests

--- tests/integration.sh
+++ tests/integration.sh
@@ -40,13 +40,14 @@ poetry shell
 poetry install
 set -e
 python slide_importer/local.py  --user $AIRFLOW_USER -P $AIRFLOW_PASSWORD --server-url http://localhost:8080  -p '{ "tissue-high-level": 8, "tissue-high-filter": "tissue_low>1", "tumor-filter": "tissue_low>1", "gpu": null,"tumor-chunk": 1024}' --wait
-
+cd ..
 
 [ $(curl http://localhost:4080/ome_seadragon/arrays/list/ | jq length) == 3 ]
 [ $(curl http://localhost:4080/ome_seadragon/get/images/index/ | jq length) == 1 ]
-
 curl -X POST   --cookie-jar /tmp/cookies http://localhost:8888/api/auth/login/ -d '{"username": "$PROMORT_USER", "password": "PROMORT_PASSWORD"}'
 [ $(curl --cookie /tmp/cookies -u $PROMORT_USER:$PROMORT_PASSWORD  http://localhost:8888/api/tissue_fragments_collections/ | jq length) == 1 ]
-cd ..
+
+[ $(find data/ -name ro-crate-metadata.json | wc -l)  == 1 ] 
+
 ./compose.sh down
 
