affb6b37b64a244f58b724123bb0b4650f198ad9
Mauro Del Rio <mauro@crs4.it>
2022-02-25 15:02:22 +0100

    refactor adding proxy and backend compose files

--- compose.sh
+++ compose.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose  -f docker-compose.yaml  -f docker-compose.omero.yaml -f docker-compose.promort.yaml  $@
+docker-compose  -f docker-compose.yaml -f docker-compose.override.yml  -f docker-compose.backend.yaml -f docker-compose.backend.override.yml $@
