b384da2e53c4749a5b4d305bf5aec7d2de9d5cc0
Mauro Del Rio <mauro@crs4.it>
2022-03-01 10:39:58 +0100

    add PROXY_PORT,fix promort config

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 PROJECT=$1
-git clone https://github.com/crs4/deephealth-pipelines.git $PROJECT
+REPO=${2:-git@github.com:mdrio/deephealth-pipelines.git}
+BRANCH=${3:-develop}
+
+git clone $REPO $PROJECT
 cd $PROJECT
+git checkout $BRANCH
 ./create_env.sh
 sed -i "s|PROJECT=dev|PROJECT=$(basename $PROJECT)|g" .env
 sed -i "s|DOCKER_NETWORK=deephealth|DOCKER_NETWORK=deephealth-$(basename $PROJECT)|g" .env
+sed  -i "s|omeseadragon:4080|${PROJECT}.omenginx.local:${PROXY_PORT}|g" promort_config/config.yaml
